48544c55faa6695f3048c488280cd1436ebfd3bc
Udayan Maurya <edmauryaishu@gmail.com>
2024-05-10 13:29:34 -0700

    test chane

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require("express");
+const morgan = require("morgan");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // Initialise morgan's middleware
-app.use(morgan("tiny"))
+app.use(morgan("tiny"));
 
 // Root path handler
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    "path": "/",
-    "response": "You've hit the root path."
-  })
-})
+    path: "/",
+    response: "You've hit the root path!",
+  });
+});
 
 // Healthcheck handler
-app.get('/healthz', (req, res) => {
-    res.json({
-        "status": "OK"
-    })
-})
+app.get("/healthz", (req, res) => {
+  res.json({
+    status: "OK",
+  });
+});
 
 // Boot up Express
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
